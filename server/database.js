077eeeb7adb2008caa4a2e72f98ee1cd5c542d8d
Nahuel D. Bogado <ndevbogado@gmail.com>
2022-03-10 15:07:33 -0300

    Added Admin pages to server directory

--- server/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require ('mongoose');
-const URI = process.env.MONGODB_URI; //crea la base de datos si no está creada
-mongoose.connect (URI, {useNewUrlParser: true, useUnifiedTopology: true });
-
-const connection = mongoose.connection;
-connection.once ('open', ()=> {console.log ('Database Connected');})
